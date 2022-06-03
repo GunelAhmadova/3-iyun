d969439a70964d02994588830f4238385dc95842
txt5 <105361005+GunelAhmadova@users.noreply.github.com>
2022-06-03 11:09:09 +0400

    Add files via upload

---
+++ ClassLibrary2/Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary2
+{
+    class Department
+    {
+        public string Name;
+        public double EmployeLimit;
+        public double SallaryLimit;
+
+        public Employee[] Employees = new Employee[0];
+        
+        public void AddEmployee(Employee employee1)
+        {
+            Array.Resize(ref Employees, Employees.Length + 1);
+            Employees[Employees.Length - 1] = employee1;
+        }
+       
+    }
+}
---
+++ ClassLibrary2/Employee.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary2
+{
+    class Employee
+    {
+        private string _name;
+        private string _surname;
+        private int _salary; //Employee Name və Surname dəyərləri 
+                             //yalnız hərflərdən ibarət olmalıdır və
+                             //Böyük hərflə başlamalıdır
+
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                bool check = false;
+                if (Char.IsUpper(value[0]))
+                {
+
+                    for (int i = 0; i < value.Length; i++)
+                    {  
+                        if (Char.IsLetter(value[i]))
+                        {
+                            check = true;
+                           
+                        }
+                        else
+                        {
+                            check = false;
+                            break;
+
+                        }
+                        
+                        
+                    }
+
+                    if (check==true)
+                    {
+                        _name = value;
+                    }
+                } 
+
+
+
+
+            }
+        }
+        public string SurName
+        {
+            get
+            {
+                return _surname;
+            } 
+            set
+            {
+                bool check = false;
+                if (Char.IsUpper(value[0]))
+                {
+                    for (int i = 0; i < value.Length; i++)
+                    {
+                        if (Char.IsLetter(value[i]))
+                        {
+                            check = true;
+                        }
+                        else
+                        {
+                            check = false;
+                            break;
+                        }
+                    }
+                    if (check == true)
+                    {
+                        _surname = value;
+                    }
+                }
+            }
+        }
+
+        public int Salary
+        {
+            get
+            {
+                return _salary;
+            } 
+            set
+            {
+                if (250 < value)
+                {
+                    _salary = value;
+                }
+            }
+        }
+    }
+}
